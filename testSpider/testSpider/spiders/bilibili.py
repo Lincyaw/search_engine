0d54a277f047e41e5bc4f769c2d3cc0bc7393206
Fay <814750204@qq.com>
2020-08-02 20:11:31 +0800

    还是不知道怎么解决

--- testSpider/testSpider/spiders/bilibili.py
+++ testSpider/testSpider/spiders/bilibili.py
@@ -14,15 +14,21 @@ class BilibiliSpider(scrapy.Spider):
     # 允许访问的域
     allowed_domains = ['bilibili.com/v/game/stand_alone/#/']
     # 爬取的起始地址
-    start_urls = ['https://www.bilibili.com/v/game/stand_alone/#/all/default/0/1/']
+    start_urls = ['https://www.bilibili.com/v/game/stand_alone/?rt=V%2FymTlOu4ow%2Fy4xxNWPUZ3XqQ%2BU%2B%2FNnablrXdXaL%2Brk%3D']
+    # start_urls = ['https://www.view.sdu.edu.cn']
 
     def parse(self, response):
         # 爬取当前网页
         print('start parse : ' + response.url)
-        # self.destination_list.remove(response.url)
-        if response.url.startswith("https://www.bilibili.com/v/game/"):
-            for box in response.xpath('//li//div[@class="l-item"]'):
-                title = box.xpath('//div[@class="r"]/a/text()').extract_first()
-                up_info = box.xpath('//div[@class="r"]/div[@class="up-info"]/a/text()').extract_first()
-                introduction = box.xpath('//div[@class ="r"]//div[1]/text()').extract_first()
-                print(title, up_info, introduction)
+        print("开始了开始了")
+        selectors = response.xpath('//*[@id="videolist_box"]/div[2]/ul')
+        print(selectors)
+        for selector in selectors:
+            title = selector.xpath('.//li/div[@class="l-item"]//div[@class="r"]/a/text()')
+            up_info = selector.xpath('.//li/div[@class="l-item"]//div[@class="r"]/div[@class="up-info"]/a/text()')
+            introduction = selector.xpath('.//li/div[@class="l-item"]//div[@class="r"]/div[1]/text()')
+            print(title, up_info, introduction)
+        print("结束了")
+
+
+
